2bee1f0639ec0fcd1e8766e36f47944976392263
m5fisker <sooryaku1@live.co.uk>
2021-09-07 14:24:02 +1000

    Update EchoAR.swift

--- SwitftUI_AR/EchoAR.swift
+++ SwitftUI_AR/EchoAR.swift
@@ -19,7 +19,7 @@ import Foundation
 
 
 class EchoAR{
-    let api_key="nameless-field-7586" //insert your echoAR API key here
+    let api_key="" //insert your echoAR API key here
     //echoar endpoint
     var serverURL="https://console.echoar.xyz/query?key="
     //websocket endpoint
